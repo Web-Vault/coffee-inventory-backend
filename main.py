cb371bb781c0d8aaf89b492e40b8f7b85d806df0
Aryan Soni <web.vault0516@gmail.com>
2026-05-12 13:53:55 +0530

    added database imports

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from sqlalchemy.orm import Session
+from database import get_db, engine, Base
 app = FastAPI()
 
 app.add_middleware(
