0e9cef2e99ccb6e0386b91f89b2a951eca81d924
Aryan Soni <web.vault0516@gmail.com>
2026-05-12 13:59:24 +0530

    added database imports

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
+
 from database import get_db, engine, Base
+import models
+import schemas
+import parser
+
 app = FastAPI()
 
 app.add_middleware(
@@ -13,5 +18,5 @@ app.add_middleware(
 )
 
 @app.get("/")
-def home():
-    return {"message": "Backend running"}
+def read_root():
+    return {"message": "Welcome to BrewIQ API"}
